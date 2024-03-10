bf032580123714fb1111c14e66fd0f4e99d713c9
HikariTempest <arwed@gmx.at>
2024-03-10 15:46:54 +0100

    test if I can get all git tags with github actions

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 pushd test-app
-  ./mvnw clean package
+  ./mvnw install -Dnative
   ls -l target
 popd
 
 mkdir -p dist
-cp test-app/target/*-SNAPSHOT.jar dist/backend.jar
+cp test-app/target/*-runner dist/backend-runner
 ls -l dist
 
